0ba4683b8e88eecba41e485dda4fb72702d44238
SingleNight <1035258861@qq.com>
2016-05-17 14:54:19 +0800

    Initial Commit

---
+++ LYCircleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYCircleView
+//
+//  Created by user on 16/5/17.
+//  Copyright © 2016年 ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYCircleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYCircleView
+//
+//  Created by user on 16/5/17.
+//  Copyright © 2016年 ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
